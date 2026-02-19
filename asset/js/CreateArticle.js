54f31d8b23baf58b922821d082808755061fed0a
chouyoura070-alt <chouyoura070@gmail.com>
2026-02-19 15:39:22 +0700

    pull login script

---
+++ asset/js/CreateArticle.js
@@ -0,0 +1,13 @@
+//get Element from user input 
+let GetTitle = document.getElementById("title").value;
+console.log(GetTitle);
+let GetCategorySelect = document.getElementById("categorySelect").value;
+console.log(GetCategorySelect);
+let GetThumbnail = document.getElementById("thumbnail").value;
+console.log(GetThumbnail);
+let GetContent = document.getElementById("content").value;
+console.log(GetContent);
+// Error message 
+document.getElementById("titleError").innerHTML = "";
+document.getElementById("categoryError").innerHTML = "";
+document.getElementById("contentError").innerHTML = "";

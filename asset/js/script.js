d3bca28dba76424a4b73bbc65b3d9e6317ab5ca6
Chum-Phula <phulachum2003@gmail.com>
2026-02-23 16:36:32 +0700

    Update Everything

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,6 @@
+let token = localStorage.getItem("token") || sessionStorage.getItem("token");
+
+
 // មុខងារសម្រាប់បង្ហាញ Modal ពេលចុចលើប៊ូតុង Logout ក្នុង Sidebar ឬ Profile Dropdown
 document.querySelectorAll(".logout-trigger").forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -18,3 +21,25 @@ document.getElementById("confirmLogout")?.addEventListener("click", () => {
   // ២. បញ្ជូនអ្នកប្រើប្រាស់ទៅកាន់ទំព័រ Login
   window.location.href = "../index.html";
 });
+
+
+// ============ Get Profile ========================================
+function getProfile(){
+       fetch('https://blogs2.csm.linkpc.net/api/v1/auth/profile',{
+        method : 'GET',
+        headers : {
+          'Content-Type' : 'application/json',
+          'Authorization' : `Bearer ${token}`
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById('avatarUser').src = data.data.avatar;
+        document.getElementById('profileUser').textContent = data.data.firstName + " " + data.data.lastName;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+    getProfile();
